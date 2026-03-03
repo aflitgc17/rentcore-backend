f7ba8f9de9a8149d9c32cc5d839505d13cca6c2a
cdgds17 cmpt <cdgds17cmpt@abcd.local>
2026-03-03 17:08:08 +0900

    3/3-8

--- src/app.js
+++ src/app.js
@@ -21,14 +21,6 @@ const facilityReservationRoutes = require("./routes/facilityReservations");
 
 const prisma = new PrismaClient();
 const app = express();
-// const PORT = 4000;
-
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000",
-//     credentials: true,
-//   })
-// );
 
 const allowedOrigins = [
   "http://localhost:3000",
